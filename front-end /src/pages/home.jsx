070f7fd8a7271ddae4b97cf58df1f69dade0fec5
Phillip Truong <TruongHPhillip@gmail.com>
2022-04-06 21:57:18 -0400

    added todo checkbox with use state

--- front-end /src/pages/home.jsx
+++ front-end /src/pages/home.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
 
-  const fakeTodoList = [
-    {itemString: 'eat', completed: false},
-    {itemString: 'shop', completed: false},
-    {itemString: 'call', completed: false},
-  ]
-  return (
+  const [todoList, setTodoList] = useState([
+    { itemString: 'eat', completed: true, id: '1' },
+    { itemString: 'shop', completed: false, id: '2' },
+    { itemString: 'call', completed: false, id: '3' },
+  ])
 
+  const handleTodoListChange = (e) => {
+    setTodoList(
+      todoList.map((todoItem) => {
+        return todoItem.id === e.target.id ? { ...todoItem, completed: !todoItem.completed } : todoItem;
+      })
+    );
+    console.log(todoList)
+  }
+
+  return (
     <div className='min-h-screen bg-blue-200 p-10 grid gap-5 grid-rows-5 grid-cols-1 sm:grid-cols-3 sm:grid-rows-1'>
       <div className='p-5 bg-white rounded-lg'>
         <h2>My Lists:</h2>
@@ -23,10 +32,18 @@ const Home = () => {
           List Name
         </h2>
 
-        <div className="flex justify-center items-center bg-orange-300">
+        <div className="flex justify-center items-center">
           <div className="flex flex-col pt-8 lg:w-2/5 sm:w-3/5 w-11/12 gap-4">
-            {fakeTodoList.map((item, index) => (
-              <p key={index}>{item.itemString}</p>
+            {todoList.map((item, index) => (
+              <div key={index} className="flex gap-2 items-center">
+                <input
+                  id={item.id}
+                  type="checkbox"
+                  checked={item.completed}
+                  onChange={handleTodoListChange}
+                />
+                <p>{item.itemString}</p>
+              </div>
             ))}
 
             <div className="flex gap-2">
@@ -35,22 +52,11 @@ const Home = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               />
               <button
-                className={`bg-blue-500 text-white font-bold py-2 px-4 rounded`}
+                className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
               >
-                Save
+                Add
               </button>
             </div>
-            {/* <div className="flex items-center gap-2">
-              <div
-                className={`flex flex-grow items-center gap-2 bg-gray-200 p-2 rounded cursor-pointer`}
-              >
-                <input className="flex-none" type="checkbox" />
-                <div className="flex-grow">Grab a coffee</div>
-              </div>
-              <button className="flex-none bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                Delete
-              </button>
-            </div> */}
           </div>
         </div>
       </div>
