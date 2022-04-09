8f60bae6d780aae516832b2597e927fc853617e1
Phillip Truong <TruongHPhillip@gmail.com>
2022-04-09 19:30:37 -0400

    removed reference to todo items in list

--- back-end/models/todoList.js
+++ back-end/models/todoList.js
@@ -1,20 +1,10 @@
 const mongoose = require('mongoose')
-// const todoListItemSchema = require('./todoListItem').schema
-
-const todoListItemSchema = new mongoose.Schema({
-  itemString: {
-    type: String,
-    required: true
-  },
-  completed: Boolean
-})
 
 const todoListSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
   },
-  listItems: [todoListItemSchema]
 })
 
 module.exports = mongoose.model('TodoList', todoListSchema)
