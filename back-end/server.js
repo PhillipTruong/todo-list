4047dd8c6e50fe3091bcfec93b075edc3c8c464c
Phillip Truong <TruongHPhillip@gmail.com>
2022-04-10 13:45:16 -0400

    added cors quick fix

--- back-end/server.js
+++ back-end/server.js
@@ -10,6 +10,14 @@ db.on('open', () => console.log('connected to database'))
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
 
